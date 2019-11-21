485b2f8a884426c28eaa22a3f40ef30e0e7508cb
Kelsey Cole <kelsey.cole@uky.edu>
2019-11-21 15:07:44 -0500

    added another not found function

--- src/test/cases/app-test.js
+++ src/test/cases/app-test.js
@@ -15,4 +15,15 @@ describe('GET - /', async () => {
         expect(response.text).to.equal('Not Found')
     })
 
+    it('returns 404 again', async () => {
+        // make a GET request on the app for the url '/'
+        const response2 = await request(app).get('/2')
+
+        // check the HTTP status code
+        expect(response2.statusCode).to.equal(404)
+        // check the raw text returned
+        expect(response2.text).to.equal('Not Found Again :-(')
+    })
+
+
 })
